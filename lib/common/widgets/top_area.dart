3194a63cb3f534f85824c82e3afd9e0ae695879c
오우영 <fz7948@gmail.com>
2023-03-07 17:11:07 +0900

    feat: top area widget 추가

--- lib/common/widgets/top_area.dart
+++ lib/common/widgets/top_area.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:mutyne/constants/sizes.dart';
-import 'package:mutyne/constants/colors.dart';
 
 class TopArea extends StatelessWidget {
   const TopArea({super.key});
 
+  void _onNavigateBack(BuildContext context) {}
+
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: Sizes.size58,
-      color: BaseColors.black[2],
-      child: Row(
-        children: const [],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size6,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () => _onNavigateBack(context),
+              child: Image.asset('assets/arrow_back.png'),
+            ),
+          ],
+        ),
       ),
     );
   }
